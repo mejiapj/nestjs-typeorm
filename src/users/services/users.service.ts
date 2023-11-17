a6d73c691d4f2a675552cc6e0397bc4e2bfe4d99
Jorge L. Mejia Pejerrey <mejiapj@gmail.com>
2023-11-17 18:45:16 -0500

    Clase 11/36 -> TypeORM: active record vs. repositories

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -69,13 +69,12 @@ export class UsersService {
     this.users.splice(index, 1);
     return true;
   }
-
-  getOrderByUser(id: number): Order {
+  async getOrderByUser(id: number) {
     const user = this.findOne(id);
     return {
       date: new Date(),
       user,
-      products: this.productsService.findAll(),
+      products: await this.productsService.findAll(),
     };
   }
 
